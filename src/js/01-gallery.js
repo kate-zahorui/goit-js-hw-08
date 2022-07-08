a7fcb8c8a56d462ea9b852c4d1b72a83a6fd83b2
kate-zahorui <kate.zahorui@gmail.com>
2022-07-08 20:47:33 +0300

    Task 01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div>
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>
+</div>`;
+  })
+  .join('');
+
+galleryRef.innerHTML = galleryItemsMarkup;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250ms',
+});
