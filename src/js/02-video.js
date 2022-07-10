2b531f361a7c575765b16361555516d654a688e3
kate-zahorui <kate.zahorui@gmail.com>
2022-07-10 16:14:05 +0300

    Task 02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const savedTime = Number(localStorage.getItem('videoplayer-current-time'));
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+const onTimeUpdate = function (event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
